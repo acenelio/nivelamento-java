146534b897fd40682ab3b752bf399e0ade2b6f46
Nelio Alves <acenelio@gmail.com>
2018-11-25 17:08:18 -0200

    Fix uri1143

--- src/uri1143.java
+++ src/uri1143.java
@@ -9,15 +9,12 @@ public class uri1143 {
 
 		int n = sc.nextInt();
 		
-		int inicio = 1;
-		
 		for (int i=1; i<=n; i++) {
-			
-			int segundo = inicio + 1;
-			int terceiro = inicio + 2;
-			System.out.printf("%d %d %d PUM%n", inicio, segundo, terceiro);
 
-			inicio = inicio + 4;
+			int primeiro = i;
+			int segundo = i * i;
+			int terceiro = i * i * i;
+			System.out.printf("%d %d %d%n", primeiro, segundo, terceiro);
 		}
 		
 		sc.close();
