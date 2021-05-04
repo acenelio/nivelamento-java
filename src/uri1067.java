138d49010166fde083a12bb9d013fe1417af9b41
Abner Eduardo Ferreira <abenaru@pm.me>
2021-05-04 12:00:53 -0300

    Fix uri1067.java
    O exercício do curso pede que o usuário insira um valor x para então imprimir no console os números ímpares dentro do intervalo (1, x) para 1 ≤ x ≤ 1000. O código original não verifica se x realmente está dentro do intervalo pré-requisitado pelo comando do exercício. Este commit corrige o erro da solução promovida pelo professor do curso.

--- src/uri1067.java
+++ src/uri1067.java
@@ -3,17 +3,26 @@ import java.util.Scanner;
 public class uri1067 {
 
 	public static void main(String[] args) {
+		Scanner scanner;
+		int intervaloMin, intervaloMax, x;
+		boolean xRespeitaIntervalo;
 
-		Scanner sc = new Scanner(System.in);
+		intervaloMin = 1;
+		intervaloMax = 1000;
 
-		int x = sc.nextInt();
-		
-		for (int i=1; i<=x; i++) {
-			if (i % 2 != 0) {
-				System.out.println(i);
+		scanner = new Scanner(System.in);
+
+		x = scanner.nextInt();
+		xRespeitaIntervalo = intervaloMin <= x && x <= intervaloMax;
+
+		for (int numIteracao = 1; xRespeitaIntervalo && numIteracao <= x; numIteracao++) {
+			boolean numIteracaoEhImpar = numIteracao % 2 != 0;
+			if (numIteracaoEhImpar) {
+				System.out.println(numIteracao);
 			}
 		}
-		
-		sc.close();
+
+		scanner.close();
 	}
+
 }
